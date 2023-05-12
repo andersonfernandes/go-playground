80dc254dcf291bab001d741abe58fdcb8df0d164
Anderson Fernandes <fernandesanderson14@gmail.com>
2023-05-11 21:21:39 -0300

    Add newDeck func
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- cards/main.go
+++ cards/main.go
@@ -1,17 +1,7 @@
 package main
 
 func main() {
-  // card := "Ace of spades"
-  // var card string = "Ace of spades"
-
-  // card := newCard()
-
-  cards := deck{"Ace of Diamonds", newCard()}
-  cards = append(cards, "Six of Spades")
+  cards := newDeck()
 
   cards.print()
-}
-
-func newCard() string {
-  return "Five of Diamonds"
 }
