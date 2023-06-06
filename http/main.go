19015a455103181c162cbda37aa173e79821de71
Anderson Fernandes <fernandesanderson14@gmail.com>
2023-06-05 21:08:48 -0300

    Add http module
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

---
+++ http/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("https://google.com")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+  // body := make([]byte, 99999)
+  // resp.Body.Read(body)
+  //
+	// fmt.Println(string(body))
+
+  io.Copy(os.Stdout, resp.Body)
+}
