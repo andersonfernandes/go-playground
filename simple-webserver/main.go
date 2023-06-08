ea8ad6033ba3097be676a15e7c91a78ea7a20047
Anderson Fernandes <fernandesanderson14@gmail.com>
2023-06-08 19:09:02 -0300

    Update simple-webserver making it reusable
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- simple-webserver/main.go
+++ simple-webserver/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"go-playground/simple-webserver/pkg/restserever"
+	"log"
+	"net/http"
+)
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("%s received at /\n", r.Method)
+
+	rb := restserever.ResponseBody{}
+	s := http.StatusOK
+	if r.Method != http.MethodGet {
+		rb.Message = "Method not allowed!"
+		s = http.StatusMethodNotAllowed
+	} else {
+		rb.Message = "Hello World!!"
+		rb.Data = map[string]string{"foo": "Bar"}
+	}
+
+	rb.WriteJsonResponse(w, s)
+}
+
 func main() {
-	initServer()
+  server := restserever.Server{
+    Mux: *http.NewServeMux(),
+    Port: "8088",
+  }
+
+  server.Mux.HandleFunc("/", rootHandler)
+  
+  server.Start()
 }
---
+++ simple-webserver/pkg/restserever/server.go
@@ -0,0 +1,17 @@
+package restserever
+
+import (
+  "fmt"
+  "net/http"
+)
+
+type Server struct {
+  Mux http.ServeMux
+  Port string
+}
+
+func (s *Server) Start() {
+  fmt.Println("Server started at http://localhost:" + s.Port + ".")
+  fmt.Print("Press ctrl + c to stop.\n\n")
+  http.ListenAndServe(":"+s.Port, &s.Mux)
+}
